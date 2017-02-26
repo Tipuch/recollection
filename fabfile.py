e40e9c7153cdf802de15287d13cf03fccc6f5d32
tipuch <fiorini751@gmail.com>
2017-02-25 21:26:08 -0500

    added deployment script

---
+++ fabfile.py
@@ -0,0 +1,20 @@
+from __future__ import with_statement
+import os
+from fabric.api import *
+from fabric.contrib.console import confirm
+
+
+env.hosts = ['tipuch@158.69.206.24']
+base_dir = "/home/tipuch"
+working_dir = os.path.join(base_dir, "projects/recollection/")
+virtual_env = os.path.join(base_dir, "virtualenvs/recollection_env/bin/")
+uwsgi_ini = "recollection.ini"
+
+
+def deploy():
+    with cd(working_dir):
+        with prefix("source %sactivate" % virtual_env):
+            run("git pull origin master")
+            run("python manage.py collectstatic --noinput")
+            run("python manage.py migrate")
+            run("touch %s" % uwsgi_ini)
