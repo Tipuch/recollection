adcfd14a9996db2349a3c32b50c85fb941ef65f7
tipuch <fiorini751@gmail.com>
2017-02-26 00:21:16 -0500

    - fixed fabfile - added dependencies with fabric3

--- fabfile.py
+++ fabfile.py
@@ -1,7 +1,6 @@
 from __future__ import with_statement
 import os
-from fabric.api import *
-from fabric.contrib.console import confirm
+from fabric.api import cd, prefix, run, env
 
 
 env.hosts = ['tipuch@158.69.206.24']
@@ -15,6 +14,7 @@ def deploy():
     with cd(working_dir):
         with prefix("source %sactivate" % virtual_env):
             run("git pull origin master")
+            run("pip install -Ur requirements.txt")
             run("python manage.py collectstatic --noinput")
             run("python manage.py migrate")
             run("touch %s" % uwsgi_ini)
