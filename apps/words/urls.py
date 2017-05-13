acf4b4861aa3f42e1d10a328595de029c8e7193a
tipuch <fiorini751@gmail.com>
2017-05-13 12:24:24 -0400

    - WIP - added urls for uploading japanese words and english words.

--- apps/words/urls.py
+++ apps/words/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import url
 
-from .views import ConvertAllReadingsView
+from .views import ConvertAllReadingsView, WordsUploadView
 
 urlpatterns = [
-    url(r'^readings/convert', ConvertAllReadingsView.as_view(),
+    url(r'^readings/convert/$', ConvertAllReadingsView.as_view(),
         name='convert_all_readings'),
+    url(r'^upload/en/$', WordsUploadView.as_view(lang='en'), name="upload_en"),
+    url(r'^upload/jp/$', WordsUploadView.as_view(lang='jp'), name="upload_jp")
 ]
