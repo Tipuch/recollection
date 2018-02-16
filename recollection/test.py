37d399af366971402eb99b306b8795b18944607c
JP <fiorini751@gmail.com>
2018-02-16 12:43:50 -0500

    - completed the test settings.

--- recollection/test.py
+++ recollection/test.py
@@ -23,6 +23,18 @@ DATABASES = {
     }
 }
 
+CACHES = {
+    'default': {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/1",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    }
+}
+
+SESSION_ENGINE = 'django.contrib.sessions.backends.cache'
+
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
